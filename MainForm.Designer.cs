f71f2f40e633b1d87ebba458cb626b831019ba3f
Space Time <Zeus6_6@163.com>
2021-10-16 20:44:17 +0800

    添加项目文件。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Sheath_Unblocker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UnblockButton = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.NavigateButton = new System.Windows.Forms.Button();
+            this.RecurseCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // UnblockButton
+            // 
+            this.UnblockButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.UnblockButton.ForeColor = System.Drawing.Color.Black;
+            this.UnblockButton.Location = new System.Drawing.Point(633, 12);
+            this.UnblockButton.Margin = new System.Windows.Forms.Padding(0);
+            this.UnblockButton.Name = "UnblockButton";
+            this.UnblockButton.Size = new System.Drawing.Size(150, 46);
+            this.UnblockButton.TabIndex = 2;
+            this.UnblockButton.Text = "解锁";
+            this.UnblockButton.UseVisualStyleBackColor = true;
+            this.UnblockButton.Click += new System.EventHandler(this.UnblockButton_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.PathTextBox.BackColor = System.Drawing.Color.White;
+            this.PathTextBox.ForeColor = System.Drawing.Color.Black;
+            this.PathTextBox.Location = new System.Drawing.Point(16, 16);
+            this.PathTextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.PathTextBox.MaxLength = 260;
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.ReadOnly = true;
+            this.PathTextBox.Size = new System.Drawing.Size(437, 38);
+            this.PathTextBox.TabIndex = 0;
+            // 
+            // NavigateButton
+            // 
+            this.NavigateButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.NavigateButton.ForeColor = System.Drawing.Color.Black;
+            this.NavigateButton.Location = new System.Drawing.Point(468, 12);
+            this.NavigateButton.Margin = new System.Windows.Forms.Padding(0);
+            this.NavigateButton.Name = "NavigateButton";
+            this.NavigateButton.Size = new System.Drawing.Size(150, 46);
+            this.NavigateButton.TabIndex = 1;
+            this.NavigateButton.Text = "浏览";
+            this.NavigateButton.UseVisualStyleBackColor = true;
+            this.NavigateButton.Click += new System.EventHandler(this.NavigateButton_Click);
+            // 
+            // RecurseCheckBox
+            // 
+            this.RecurseCheckBox.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.RecurseCheckBox.AutoSize = true;
+            this.RecurseCheckBox.Location = new System.Drawing.Point(798, 18);
+            this.RecurseCheckBox.Margin = new System.Windows.Forms.Padding(0);
+            this.RecurseCheckBox.Name = "RecurseCheckBox";
+            this.RecurseCheckBox.Size = new System.Drawing.Size(166, 35);
+            this.RecurseCheckBox.TabIndex = 3;
+            this.RecurseCheckBox.Text = "遍历子目录";
+            this.RecurseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.UnblockButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(979, 69);
+            this.Controls.Add(this.RecurseCheckBox);
+            this.Controls.Add(this.NavigateButton);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.UnblockButton);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 140);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sheath Unblocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UnblockButton;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button NavigateButton;
+        private System.Windows.Forms.CheckBox RecurseCheckBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Sheath_Unblocker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UnblockButton_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(PathTextBox.Text))
+                Text = "解锁中...";
+            else
+                Text = "文件路径错误";
+
+            Process unblockProcess = new System.Diagnostics.Process();
+
+            #region 配置unblockProcess的参数
+            unblockProcess.StartInfo.FileName = "cmd.exe";
+            unblockProcess.StartInfo.Arguments = RecurseCheckBox.Checked ?
+                @"/c cd /d """ + PathTextBox.Text + @""" && powershell -command ""Get-ChildItem -Recurse | Unblock-File""" :
+                @"/c cd /d """ + PathTextBox.Text + @""" && powershell -command ""Get-ChildItem | Unblock-File""";
+            unblockProcess.StartInfo.CreateNoWindow = true; //不显示程序窗口 
+            unblockProcess.StartInfo.UseShellExecute = false;
+            unblockProcess.StartInfo.RedirectStandardInput = true;  //接受调用程序的输入信息 
+            unblockProcess.StartInfo.RedirectStandardOutput = true; //由调用程序获取输出信息 
+            //unblockProcess.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            #endregion
+
+            unblockProcess.Start();
+            unblockProcess.WaitForExit(60 * 1000);
+            System.IO.StreamReader streamReader = unblockProcess.StandardOutput;
+            string feedBack = streamReader.ReadToEnd();
+            unblockProcess.Close();
+
+            if (feedBack == "")
+                Text = "解锁成功";
+            else
+                Text = "解锁失败";  //feedBack
+        }
+        private void NavigateButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            #region 配置folderBrowserDialog的参数
+            folderBrowserDialog.Description = "Sheath Importer";
+            folderBrowserDialog.UseDescriptionForTitle = true;  //修改标题
+            folderBrowserDialog.AutoUpgradeEnabled = true;   //自动升级对话框样式
+            #endregion
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                PathTextBox.Text = folderBrowserDialog.SelectedPath; //路径
+            }
+        }
+    }
+}
